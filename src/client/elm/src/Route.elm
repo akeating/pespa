7d8be5e2753d6fe6765e9e74039ef4caea816b65
Adam Keating <1427265+akeating@users.noreply.github.com>
2018-01-10 09:02:45 -0800

    Use routes

---
+++ src/client/elm/src/Route.elm
@@ -0,0 +1,62 @@
+module Route exposing (Route(..), fromLocation, href, modifyUrl)
+
+import Html exposing (Attribute)
+import Html.Attributes as Attr
+import Navigation exposing (Location)
+import UrlParser as Url exposing ((</>), Parser, oneOf, parseHash, s, string)
+
+
+-- ROUTING --
+
+
+type Route
+    = Home
+    | Login
+
+
+route : Parser (Route -> a) a
+route =
+    oneOf
+        [ Url.map Home (s "")
+        , Url.map Login (s "login")
+        ]
+
+
+
+-- INTERNAL --
+
+
+routeToString : Route -> String
+routeToString page =
+    let
+        pieces =
+            case page of
+                Home ->
+                    []
+
+                Login ->
+                    [ "login" ]
+
+    in
+    "/" ++ String.join "/" pieces
+
+
+
+-- PUBLIC HELPERS --
+
+
+href : Route -> Attribute msg
+href route =
+    Attr.href (routeToString route)
+
+
+modifyUrl : Route -> Cmd msg
+modifyUrl =
+    routeToString >> Navigation.modifyUrl
+
+
+fromLocation : Location -> Maybe Route
+fromLocation location =
+    case location.pathname of
+        _ ->
+            parseHash route location
