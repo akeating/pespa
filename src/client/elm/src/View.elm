7cfaa927a188a24985ee74dfa8b4b94a553b7efb
Adam Keating <1427265+akeating@users.noreply.github.com>
2018-01-18 21:37:48 -0800

    Add basic snackbar

--- src/client/elm/src/View.elm
+++ src/client/elm/src/View.elm
@@ -6,7 +6,7 @@ import Html.Events exposing (..)
 import Navigation exposing (Location)
 import Route exposing (Route, fromLocation)
 import Types exposing (..)
-import Common.Frame as Frame
+import Feature.Frame as Frame
 import Feature.Home as Home
 import Feature.Login as Login
 import Feature.Content as Content
@@ -15,21 +15,21 @@ import Feature.Content as Content
 view : Model -> Html Msg
 view model =
     let
-        { context, homeModel, contentModel, loginModel } = model
+        { context, homeModel, contentModel, loginModel, frameModel } = model
         maybeRoute = fromLocation context.location
     in
         case maybeRoute of
             Just Route.Login ->
-                Frame.view model (Login.view loginModel context)
+                Frame.view frameModel (Login.view loginModel context)
 
             Just Route.Home ->
-                Frame.view model (Home.view homeModel context)
+                Frame.view frameModel (Home.view homeModel context)
 
             Just Route.Content ->
-                Frame.view model (Content.view contentModel context)
+                Frame.view frameModel (Content.view contentModel context)
 
             _ ->
-                Frame.view model (Home.view homeModel context)
+                Frame.view frameModel (Home.view homeModel context)
 
 
 viewLocation : Location -> Html Msg
